730d39d336f61a05328435a2f74be8b8b720a307
Ashiph <ashiphs@cybage.com>
2023-10-17 17:32:50 +0530

    Changed packaging from JAR to WAR

--- src/main/java/com/springrest/courseapplication/CourseapplicationApplication.java
+++ src/main/java/com/springrest/courseapplication/CourseapplicationApplication.java
@@ -2,9 +2,10 @@ package com.springrest.courseapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.SpringServletContainerInitializer;
 
 @SpringBootApplication
-public class CourseapplicationApplication {
+public class CourseapplicationApplication extends SpringServletContainerInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CourseapplicationApplication.class, args);
